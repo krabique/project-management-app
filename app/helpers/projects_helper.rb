640e230f7975bcd1ad2d281b4bca7c5a8780e4e8
Oleg Larkin <krabique48@gmail.com>
2017-06-10 16:34:47 +0000

    Add support for non-image files in documents

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,9 +1,25 @@
 module ProjectsHelper
+  def cloudinary_start_path
+    return "https://res.cloudinary.com/#{ENV['CLOUDINATY_CLOUD_NAME']}/"
+  end
+  
   def link_to_cloudinary(path, parameters)
-    "https://res.cloudinary.com/krabique48/image/upload/#{parameters}/#{path}"
+    type = cloudinary_link_type(path)
+    #cloudinary_start_path << ("#{type}/upload/#{parameters}/#{path}")
+    "#{cloudinary_start_path}#{type}/upload/#{parameters}/#{path}"
   end
   
   def link_to_cloudinary(path)
-    "https://res.cloudinary.com/krabique48/image/upload/#{path}"
+    type = cloudinary_link_type(path)
+    #cloudinary_start_path << ("#{type}/upload/#{path}")
+    "#{cloudinary_start_path}#{type}/upload/#{path}"
+  end
+  
+  def is_image_path?(path)
+    path =~ /\.(?:jpg|gif|png)/
+  end
+  
+  def cloudinary_link_type(path)
+    is_image_path?(path) ? 'image' : 'raw'
   end
 end
