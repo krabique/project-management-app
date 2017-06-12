7330bf6947a79fe9f9455c911ae5572da06c2297
Oleg Larkin <krabique48@gmail.com>
2017-06-12 20:32:39 +0000

    Remove json from projects controller and refactor it

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,8 +7,10 @@ class ProjectsController < ApplicationController
   
   def create
     if authorized?
-      respond_to do |format|
-        create_action_respond_formatted(format)
+      if @project = Project.create(project_params)
+        redirect_to @project, notice: 'Project was successfully created.'
+      else
+        render :new
       end
     end
   end
@@ -32,8 +34,10 @@ class ProjectsController < ApplicationController
   def update
     if authorized?
       params[:project][:user_ids] ||= []
-      respond_to do |format|
-        update_action_respond_formatted(format)
+      if @project.update(project_params)
+        redirect_to @project, notice: 'Project was successfully updated.'
+      else
+        render :edit
       end
     end
   end
@@ -54,43 +58,5 @@ class ProjectsController < ApplicationController
       params.require(:project).permit(:title, :description, :tag_list)
     end
   end
-  
-  def update_action_respond_formatted(format)
-    if @project.update(project_params)
-      update_action_respond_formatted_for_successful(@project, format)
-    else
-      update_action_respond_formatted_for_unchanged(@project, format)
-    end
-  end
-  
-  def update_action_respond_formatted_for_successful(project, format)
-    format.html { redirect_to project, 
-      notice: 'Project was successfully updated.' }
-    format.json { render :show, status: :ok, location: project }
-  end
-  
-  def update_action_respond_formatted_for_unchanged(project, format)
-    format.html { render :edit }
-    format.json { render json: project.errors, 
-      status: :unprocessable_entity }
-  end
-  
-  def create_action_respond_formatted(format)
-    if @project = Project.create(project_params)
-      create_action_respond_formatted_for_successful(format)
-    else
-      create_action_respond_formatted_for_unchanged(format)
-    end
-  end
-  
-  def create_action_respond_formatted_for_successful(format)
-    format.html { redirect_to @project, 
-      notice: 'Project was successfully created.' }
-    format.json { render :show, status: :created, location: @project }
-  end
-  
-  def create_action_respond_formatted_for_unchanged(format)
-    format.html { render :new }
-    format.json { render json: @project.errors, status: :unprocessable_entity }
-  end  
+
 end
