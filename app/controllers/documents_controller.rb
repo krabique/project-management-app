c3885e6a211e3216ea791e4f3a700557e0a55a57
Oleg Larkin <krabique48@gmail.com>
2017-06-12 21:19:45 +0000

    Remove json from documents controller and refactor it

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -10,8 +10,12 @@ class DocumentsController < ApplicationController
   
   def create
     if authorized?
-      respond_to do |format|
-        create_action_respond_formatted(format)
+      if preloaded_cloudinary = create_cloudinary_link
+        create_document(preloaded_cloudinary)
+        redirect_to @project, notice: 'Document was successfully created.'
+      else
+        redirect_to new_project_document_path(@project), 
+          alert: 'There was an error trying to create a documment.'
       end
     end
   end
@@ -26,8 +30,12 @@ class DocumentsController < ApplicationController
   
   def update
     if authorized?
-      respond_to do |format|
-        update_action_respond_formatted(format)
+      if update_document_transaction
+        redirect_to @document.project, 
+          notice: 'Document was successfully updated.'
+      else
+        redirect_to edit_project_document(@project, @document), 
+          alert: 'There was an error updating the document.'
       end
     end
   end
@@ -60,26 +68,6 @@ class DocumentsController < ApplicationController
       :last_editor)
   end
   
-  def update_action_respond_formatted(format)
-    if update_document_transaction
-      update_action_respond_formatted_for_successful(@document, format)
-    else
-      update_action_respond_formatted_for_unchanged(@document, format)
-    end
-  end
-  
-  def update_action_respond_formatted_for_successful(document, format)
-    format.html { redirect_to document.project, 
-      notice: 'Document was successfully updated.' }
-    format.json { render :show, status: :ok, location: document }
-  end
-  
-  def update_action_respond_formatted_for_unchanged(document, format)
-    format.html { render :edit }
-    format.json { render json: document.errors, 
-      status: :unprocessable_entity }
-  end
-  
   def update_document_transaction
     Document.transaction do
       @document.update!(document_params)
@@ -88,35 +76,12 @@ class DocumentsController < ApplicationController
     end
   end
   
-  def create_action_respond_formatted(format)
-    if preloaded_cloudinary = create_cloudinary_link
-      create_and_respond(format, preloaded_cloudinary)
-    end
-  end
-  
-  def create_and_respond(format, preloaded_cloudinary)
-    if create_document(preloaded_cloudinary)
-      create_action_respond_formatted_for_successful(format)
-    else
-      update_action_respond_formatted_for_unchanged(format)
-    end
-  end
-  
-  def create_action_respond_formatted_for_successful(format)
-    format.html { redirect_to @project, 
-      notice: 'Document was successfully created.' }
-    format.json { render :show, status: :created, location: @project }
-  end
-  
-  def update_action_respond_formatted_for_unchanged(format)
-    format.html { render :new }
-    format.json { render json: @project.errors, status: :unprocessable_entity }
-  end
-  
   def create_document(preloaded_cloudinary)
-    @project.documents.create( { title: params[:document][:title], 
-      cloudinary_uri: preloaded_cloudinary.identifier, 
-      creator: current_user.id } )
+    @project.documents.create( { 
+      title:            params[:document][:title], 
+      cloudinary_uri:   preloaded_cloudinary.identifier, 
+      creator:          current_user.id 
+    } )
   end
   
 end
