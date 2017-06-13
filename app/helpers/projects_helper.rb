6c5063346bbae9469047d85cb762f10656b93cce
Oleg Larkin <krabique48@gmail.com>
2017-06-13 22:18:48 +0000

    Move documents' representation images logic from view to helper

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -20,4 +20,23 @@ module ProjectsHelper
   def cloudinary_link_type(path)
     is_image_path?(path) ? 'image' : 'raw'
   end
+  
+  def representation_image(document)
+    if is_image_path?(document.cloudinary_uri)
+      link_to cl_image_tag(document.cloudinary_uri, :crop => :fill, 
+        :width => 120, :height => 80), 
+        link_to_cloudinary(document.cloudinary_uri), 
+        target: "_blank"
+    elsif current_user.dark_style
+      link_to image_tag('documents/default_dark.png', 
+        :width => 80, :height => 80), 
+        link_to_cloudinary(document.cloudinary_uri), 
+        target: "_blank"
+    else
+      link_to image_tag('documents/default.png', 
+        :width => 80, :height => 80), 
+        link_to_cloudinary(document.cloudinary_uri), 
+        target: "_blank"
+    end
+  end
 end
