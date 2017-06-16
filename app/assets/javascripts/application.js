953eec55c5d2eacc5390b4e9032c84d547727792
Oleg Larkin <krabique48@gmail.com>
2017-06-16 22:22:06 +0000

    Hide creator/updater info and beautify project's view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,6 +26,22 @@ $(document).ready( function(){
       $("input.cloudinary-fileupload[type=file]").cloudinary_fileupload();
     }
   });
+  
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+      acc[i].onclick = function(){
+          this.classList.toggle("active");
+          var panel = this.nextElementSibling;
+          if (panel.style.display === "block") {
+              panel.style.display = "none";
+          } else {
+              panel.style.display = "block";
+          }
+      }
+  }
+  
 });
 
 $( function() {
