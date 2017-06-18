5689d76849a22c40741c817c4a845cde4ae376ab
Oleg Larkin <krabique48@gmail.com>
2017-06-18 02:05:31 +0000

    Add authentication to ActionCable

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -7,14 +7,16 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    super
+    cookies.signed[:user_id] = current_user.id
+  end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+    cookies.delete :user_id
+  end
 
   # protected
 
